350e404355bbf23dc78f2621fe7cd173fa225e9d
Lingxiao Han <hanlingx@unc.edu>
2024-11-01 01:13:44 -0400

    updatedStartPage

---
+++ Assets/GameSettingUIScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class GameSettingUIScript : MonoBehaviour
+{
+    private static GameSettingUIScript instance;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
